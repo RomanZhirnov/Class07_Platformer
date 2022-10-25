99117e9acb4bf0a457b237c59a649ceeaf8026bd
RomanZhirnov <rvzh@mail.ru>
2022-10-25 17:52:47 +0300

    Change jamp

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -8,10 +8,10 @@ public class Controller : MonoBehaviour
 {
     [SerializeField] private LayerMask _groundLayer;
     [SerializeField] private Transform _groundCheck;
+    [SerializeField] private float _jumpForce = 15f;
+    [SerializeField] private float _speed = 2f;
 
     private Rigidbody2D _rg2D;
-    private float _speed = 2f;
-    private float _jumpForce = 10f;
     private float _distanceToCheck = 0.1f;
     private float _horizontal;
 
@@ -26,7 +26,7 @@ public class Controller : MonoBehaviour
 
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
-            _rg2D.velocity = new Vector2(_rg2D.velocity.x, _jumpForce);
+            _rg2D.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
         }
     }
 
@@ -38,10 +38,6 @@ public class Controller : MonoBehaviour
 
     private bool IsGrounded()
     {
-        //return Physics2D.OverlapCircle(_groundCheck.position, _distanceToCheck, _groundLayer);
-
-        //RaycastHit2D hit = Physics2D.Raycast(_groundCheck.position, Vector2.down, _distanceToCheck, _groundLayer);
-
         if (Physics2D.Raycast(_groundCheck.position, Vector2.down, _distanceToCheck, _groundLayer))
         {
             return true;
