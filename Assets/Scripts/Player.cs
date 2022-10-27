db4f75c543db6712c42d0a7ddb881e9b9ce91780
RomanZhirnov <rvzh@mail.ru>
2022-10-27 12:42:05 +0300

    Add Wallet and coin sound

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public event Action CoinCollect;
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (collision.collider.TryGetComponent<GoldCoin>(out GoldCoin goldCoin))
+        {
+            CoinCollect?.Invoke();
+            Destroy(collision.gameObject);
+        }
     }
 }
---
+++ Assets/Scripts/Wallet.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Wallet : MonoBehaviour
+{
+    [SerializeField] private AudioClip _takeCoin;
+    [SerializeField] private Player _coinCollect;
+
+    private AudioSource _audioSource;
+    private int _totalCoinInLevel;
+    private int _coinsInWallet;
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        GoldCoin[] _coins = GameObject.FindObjectsOfType<GoldCoin>();
+        _totalCoinInLevel = _coins.Length;
+        Debug.Log($"Tatal coins in Level: {_totalCoinInLevel}");
+    }
+    private void OnEnable()
+    {
+        _coinCollect.CoinCollect += CollectCoin;
+    }
+
+    private void OnDisable()
+    {
+        _coinCollect.CoinCollect -= CollectCoin;
+    }
+
+    private void CollectCoin()
+    {
+        _audioSource.PlayOneShot(_takeCoin);
+        _coinsInWallet++;
+        Debug.Log($"Собрано {_coinsInWallet} из {_totalCoinInLevel}");
+    }
+}
