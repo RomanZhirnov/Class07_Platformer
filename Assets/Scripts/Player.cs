3dfb0dcf40f93250f610eefa93f4aa685aaf35d2
RomanZhirnov <rvzh@mail.ru>
2022-10-28 12:46:49 +0300

    Coin change Collaider is Trigger

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public event Action CoinCollect;
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.collider.TryGetComponent<GoldCoin>(out GoldCoin goldCoin))
+        if (collision.TryGetComponent<GoldCoin>(out GoldCoin goldCoin))
         {
             CoinCollect?.Invoke();
             Destroy(collision.gameObject);
