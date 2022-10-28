8482cb09ee99ca70c8a15d5413e5c8eaa9a3883e
RomanZhirnov <rvzh@mail.ru>
2022-10-28 12:26:24 +0300

    Add to controller Move and Jump methods Add Run animation

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,8 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SpriteRenderer))]
 
 public class Controller : MonoBehaviour
 {
@@ -10,30 +10,27 @@ public class Controller : MonoBehaviour
     [SerializeField] private float _speed = 2f;
 
     private Rigidbody2D _rg2D;
-    private RaycastHit2D[] _result = new RaycastHit2D [1];
+    private RaycastHit2D[] _result = new RaycastHit2D[1];
+    private Animator _animator;
+    private SpriteRenderer _spriteR;
     private float _distanceToCheck = 0.1f;
     private float _horizontal;
+
+    static readonly int move = Animator.StringToHash("Move");
 
     void Start()
     {
         _rg2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _spriteR = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
-        _horizontal = Input.GetAxisRaw("Horizontal");
-
-        if (Input.GetButtonDown("Jump") && IsGrounded())
-        {
-            _rg2D.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
-        }
+        Move();
+        Jump();
+        _rg2D.velocity = new Vector2(_horizontal * _speed, _rg2D.velocity.y);
     }
-
-    private void FixedUpdate()
-    {
-        _rg2D.velocity = new Vector2(_horizontal * _speed, _rg2D.velocity.y);      
-    }
-
 
     private bool IsGrounded()
     {
@@ -45,5 +42,24 @@ public class Controller : MonoBehaviour
         }
 
         return false;
+    }
+
+    private void Move()
+    {
+        _horizontal = Input.GetAxis("Horizontal");
+
+        if (IsGrounded())
+        {
+            _spriteR.flipX = _horizontal < 0;
+            _animator.SetFloat(move, Mathf.Abs(_horizontal));
+        }
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            _rg2D.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+        }
     }
 }
