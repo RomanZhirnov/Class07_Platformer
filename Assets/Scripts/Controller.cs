e90c9bd9fbe652481272cca024f71ff2728e2812
RomanZhirnov <rvzh@mail.ru>
2022-10-31 12:25:34 +0300

    reroll controller

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -13,8 +13,9 @@ public class Controller : MonoBehaviour
     private Animator _animator;
     private SpriteRenderer _spriteR;
     private float _distanceToCheck = 0.1f;
-    private float _horizontal;
     private RaycastHit2D[] _result = new RaycastHit2D[1];
+    private float _xAxis;
+    private bool _haveToJump;
 
     static readonly int Run = Animator.StringToHash("Player Run");
     static readonly int Idle = Animator.StringToHash("Player Idle");
@@ -29,9 +30,42 @@ public class Controller : MonoBehaviour
 
     private void Update()
     {
-        Jump();
-        Move();
-        _rg2D.velocity = new Vector2(_horizontal * _speed, _rg2D.velocity.y);
+        _xAxis = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            _haveToJump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_xAxis != 0)
+        {
+            _rg2D.velocity = new Vector2(_xAxis * _speed, _rg2D.velocity.y);
+            _spriteR.flipX = _xAxis < 0;
+            _animator.CrossFade(Run, 0.1f);
+        }
+        else
+        {
+            _rg2D.velocity = new Vector2(0, _rg2D.velocity.y); ;
+
+            if (IsGrounded() == false)
+            {
+                _animator.CrossFade(Jumping, 0.1f);
+            }
+            else
+            {
+                _animator.CrossFade(Idle, 0.1f);
+            }
+        }
+
+        if (_haveToJump)
+        {
+            _rg2D.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+            _haveToJump = false;
+        }
+
     }
 
     private bool IsGrounded()
@@ -44,32 +78,5 @@ public class Controller : MonoBehaviour
         }
 
         return false;
-    }
-
-    private void Move()
-    {
-        _horizontal = Input.GetAxis("Horizontal");
-
-        if (IsGrounded())
-        {
-            if (_horizontal != 0)
-            {
-                _spriteR.flipX = _horizontal < 0;
-                _animator.CrossFade(Run, 0.1f);
-            }
-            else 
-            {
-                _animator.CrossFade(Idle, 0.1f);
-            }
-        }     
-    }
-
-    private void Jump()
-    {
-        if (Input.GetButtonDown("Jump") && IsGrounded())
-        {
-            _animator.CrossFade(Jumping, 0.1f);
-            _rg2D.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
-        }
     }
 }
