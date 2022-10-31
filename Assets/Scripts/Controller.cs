d50f5e82450fb800629260f3ee2e1b4435dedf01
RomanZhirnov <rvzh@mail.ru>
2022-10-31 11:03:01 +0300

    Add Cross Fade method

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -10,13 +10,15 @@ public class Controller : MonoBehaviour
     [SerializeField] private float _speed = 2f;
 
     private Rigidbody2D _rg2D;
-    private RaycastHit2D[] _result = new RaycastHit2D[1];
     private Animator _animator;
     private SpriteRenderer _spriteR;
     private float _distanceToCheck = 0.1f;
     private float _horizontal;
+    private RaycastHit2D[] _result = new RaycastHit2D[1];
 
-    static readonly int move = Animator.StringToHash("Move");
+    static readonly int Run = Animator.StringToHash("Player Run");
+    static readonly int Idle = Animator.StringToHash("Player Idle");
+    static readonly int Jumping = Animator.StringToHash("Player Jump");
 
     void Start()
     {
@@ -27,8 +29,8 @@ public class Controller : MonoBehaviour
 
     private void Update()
     {
-        Move();
         Jump();
+        Move();
         _rg2D.velocity = new Vector2(_horizontal * _speed, _rg2D.velocity.y);
     }
 
@@ -50,15 +52,23 @@ public class Controller : MonoBehaviour
 
         if (IsGrounded())
         {
-            _spriteR.flipX = _horizontal < 0;
-            _animator.SetFloat(move, Mathf.Abs(_horizontal));
-        }
+            if (_horizontal != 0)
+            {
+                _spriteR.flipX = _horizontal < 0;
+                _animator.CrossFade(Run, 0.1f);
+            }
+            else 
+            {
+                _animator.CrossFade(Idle, 0.1f);
+            }
+        }     
     }
 
     private void Jump()
     {
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
+            _animator.CrossFade(Jumping, 0.1f);
             _rg2D.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
         }
     }
